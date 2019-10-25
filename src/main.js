eb342ce6c4bcb5d7eceb4c826a77aad9019e1dde
jiming <864912785@qq.com>
2019-10-25 20:52:16 +0800

    引入vue-router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
-Vue.config.productionTip = false
-
+// 导入某一个目录，默认会去找索引文件(index.js inde.vue index.json)) 基于webpack
+// @是起的别名，指的是/src路径
+import router from '@/router'
 Vue.use(ElementUI)
-
+Vue.config.productionTip = false
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
